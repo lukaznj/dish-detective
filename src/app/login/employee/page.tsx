a5fdc1225db5b21b5527d69563f4e3547ae725fb
Luka Ražem <luka@razem.cc>
2025-11-12 21:48:08 +0100

    Made employee login page

---
+++ src/app/login/employee/page.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import React from "react";
+import { SignIn } from "@clerk/nextjs";
+import { Box, Typography } from "@mui/material";
+
+export default function EmployeeLoginPage() {
+  return (
+    <Box
+      sx={{
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        backgroundColor: "white",
+      }}
+    >
+        <Box>
+            <Typography
+                variant="h4"
+                sx={{
+                    fontWeight: 700,
+                    my: 3,
+                }}
+            >
+                Zaposlenik login
+            </Typography>
+
+            <SignIn
+                appearance={{
+                    elements: {
+                        socialButtonsBlockButton: {
+                            display: "none",
+                        },
+                        dividerRow: {
+                            display: "none",
+                        },
+                        footer: {
+                            display: "none",
+                        },
+                        formButtonPrimary: {
+                            backgroundColor: "#2869e8",
+                            '&:hover, &:focus, &:active': {
+                                backgroundColor: "#49247A",
+                            },
+                        },
+                    }}}
+                routing="path"
+                path="/login/employee"
+                signUpUrl="/login/employee"
+                forceRedirectUrl="/employee/dashboard"
+            />
+        </Box>
+
+        </Box>
+
+  );
+}
+
+
