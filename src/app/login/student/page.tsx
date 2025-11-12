b18b5361720105a701a0c85d794fde4848f1aef4
Luka Ražem <luka@razem.cc>
2025-11-12 22:14:57 +0100

    Made student login page

---
+++ src/app/login/student/page.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import React from "react";
+import { SignIn } from "@clerk/nextjs";
+import { Box, Typography } from "@mui/material";
+
+export default function EmployeeLoginPage() {
+    return (
+        <Box
+            sx={{
+                minHeight: "100vh",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                backgroundColor: "white",
+            }}
+        >
+            <Box>
+                <Typography
+                    variant="h4"
+                    sx={{
+                        fontWeight: 700,
+                        my: 3,
+                    }}
+                >
+                    Student login
+                </Typography>
+            {/*<Box sx={{*/}
+            {/*    color: "#edeff1",*/}
+            {/*}}>*/}
+                <SignIn
+                    appearance={{
+                        elements: {
+                            dividerRow: {
+                                display: "none",
+                            },
+                            footer: {
+                                display: "none",
+                            },
+                            formFieldInput: {
+                                display: "none"
+                            },
+                            formFieldLabelRow: {
+                                display: "none"
+                            },
+                            formButtonPrimary: {
+                                display: "none",
+                            },
+                            identifierField: {
+                                display: "none",
+                            },
+                            formFieldAction: {
+                                display: "none",
+                            },
+                            headerTitle: {
+                                display:"none"
+                            },
+                            headerSubtitle: {
+                                display:"none"
+                            },
+                            socialButtonsBlockButton: {
+                                height: "300%",
+                            },
+                            socialButtonsBlockButtonText: {
+                                fontSize: "1.3rem",
+                            },
+                            cardBox: {
+                                boxShadow: "none",
+                            }
+
+                        }
+                    }}
+                    routing="path"
+                    path="/login/student"
+                    signUpUrl="/login/student"
+                    forceRedirectUrl="/student/dashboard"
+                />
+            {/*</Box>*/}
+
+
+            </Box>
+
+        </Box>
+
+    );
+}
+
+
