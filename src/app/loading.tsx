130db4b6dbaf59e1e4c1b3f78058e003c37dacf4
Luka Ražem <luka@razem.cc>
2025-11-13 21:17:11 +0100

    Centered the loading component

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,5 +1,20 @@
 import PancakeStackLoader from "@/components/PancakeStackLoader";
+import { Box } from "@mui/material";
 
 export default function Loading() {
-  return <PancakeStackLoader />;
+  return (
+    <Box
+      sx={{
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        bgcolor: "#f5f5f5",
+      }}
+    >
+      <Box sx={{ width: 200, height: 200 }}>
+        <PancakeStackLoader />
+      </Box>
+    </Box>
+  );
 }
