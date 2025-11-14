ac3c08125dd00aea1601313d512490f36cfed822
Luka Ražem <luka@razem.cc>
2025-11-14 02:03:33 +0100

    Fixed some issues

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,29 +1,25 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isPublicRoute = createRouteMatcher([
-  "/",
-  "/login/student(.*)",
-  "/login/employee(.*)",
-  "/sign-in(.*)",
-  "/sign-up(.*)",
-  "/auth/redirect",
-  "/auth/sso-callback(.*)",
+    "/",
+    "/login/student(.*)",
+    "/login/employee(.*)",
+    "/sign-in(.*)",
+    "/sign-up(.*)",
+    "/auth/redirect",
+    "/auth/sso-callback(.*)",
 ]);
-// export default clerkMiddleware(async (auth, req) => {
-//   if (isPublicRoute(req)) {
-//     return;
-//   }
-
-//   await auth.protect();
-// });
 export default clerkMiddleware(async (auth, req) => {
-  // AUTH disabled for development
-  return;
+    if (isPublicRoute(req)) {
+        return;
+    }
+
+    await auth.protect();
 });
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
-  ],
+    matcher: [
+        // Skip Next.js internals and all static files, unless found in search params
+        "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+        // Always run for API routes
+        "/(api|trpc)(.*)",
+    ],
 };
